c489db386ad3092ba8d0f652c32f494d894b539b
unknown <hebertsa07@gmail.com>
2017-10-12 14:10:50 -0400

    adding assignment files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'US Time Zone Display';
+  date1;
+  buttonPST;
+  buttonMST;
+  buttonCST;
+  buttonEST;
+  timeZone(zone){
+    if(zone === "pst"){
+      this.date1 = Date.now() + (60000*(60* 3))
+      this.buttonPST = "yellow";
+      this.buttonMST = null;
+      this.buttonCST = null;
+      this.buttonEST = null;
+    } else if (zone === "mst"){
+      this.date1 = Date.now() + (60000*(60* 2))
+      this.buttonPST = null;
+      this.buttonMST = "yellow";
+      this.buttonCST = null;
+      this.buttonEST = null;
+    } else if(zone === "cst"){
+      this.date1 = Date.now() + (60000*(60* 1))
+      this.buttonPST = null;
+      this.buttonMST = null;
+      this.buttonCST = "yellow";
+      this.buttonEST = null;
+    } else if (zone === "est"){
+      this.date1 = Date.now()
+      this.buttonPST = null;
+      this.buttonMST = null;
+      this.buttonCST = null;
+      this.buttonEST = "yellow";
+    }
+  }
+  clearZone(){
+    this.date1 = null;
+    this.buttonPST = null;
+    this.buttonMST = null;
+    this.buttonCST = null;
+    this.buttonEST = null;
+  }
+  
 }
